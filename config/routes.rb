e85a033e6ae2927baa619df6f9c7d2914f56a920
Ken Sheridan <kensheridan86@gmail.com>
2023-06-01 19:58:20 +0100

    routes added, form logic started

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   root "articles#index"
-  get "/articles", to: "article#index"
-  get "/articles/:id", to: "articles#show"
+
+  resources :articles
+
+  # get "/articles", to: "article#index"
+  # get "/articles/:id", to: "articles#show"
   # Defines the root path route ("/")
   # root "articles#index"
 end
